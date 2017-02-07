8b0e5bd697cb2b680a72b873d4f581cec19cb9f1
MeikoTiger <meiko@somba.de>
2017-02-07 11:30:51 +0100

    ll

---
+++ ordner-me/haupt.cs
@@ -0,0 +1,16 @@
+// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}https://github.com/MeikoTiger/NOC-Meikohttps://github.com/MeikoTiger/NOC-Meiko
